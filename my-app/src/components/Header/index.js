ea7837ac13c42740829d1c69d12699888313421a
shar09 <sharathtelu9@gmail.com>
2020-04-08 12:59:47 -0400

    header done

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -3,7 +3,10 @@ import "./style.css";
 
 function Header() {
    return(
-      <div></div>
+      <div className="header">
+         <h1 className="title">Clicky Game!</h1>
+         <h3 className="caption">Click an image to earn points but dont click on any more than once!</h3>
+      </div>
    );
 }
 
--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function Navbar(props) {
    return (
       <div className="nav">
-         <h3><a href="">Clicky Game</a></h3>
-         <h3>{props.result}</h3>
-         <h3>Score: {props.score} | Top Score: {props.topScore}</h3>
+         <h3 className="nav-items"><a href="">Clicky Game</a></h3>
+         <h3 className="nav-items">{props.result}</h3>
+         <h3 className="nav-items">Score: {props.score} | Top Score: {props.topScore}</h3>
       </div>
    );
 }
