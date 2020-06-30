cd599bad035df55b2b5b27a50062f1da268e69d3
shar09 <sharathtelu9@gmail.com>
2020-06-30 09:22:03 -0400

    Updates

---
+++ precache-manifest.52dc0c4c61775a0d2760b5585a64b589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9251533ea247746a980a4294acc5b829",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "c11a9196f4ccaa3e2764",
+    "url": "/Clicky/static/css/main.53868e2d.chunk.css"
+  },
+  {
+    "revision": "1cf306cbe531c1f2568b",
+    "url": "/Clicky/static/js/2.359d757b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky/static/js/2.359d757b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11a9196f4ccaa3e2764",
+    "url": "/Clicky/static/js/main.7bde977b.chunk.js"
+  },
+  {
+    "revision": "ea2731753c5f450456da",
+    "url": "/Clicky/static/js/runtime-main.3e956f12.js"
+  }
+]);
