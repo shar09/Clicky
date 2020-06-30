db86040e69168377a1d53fe784ab16a8a791917c
shar09 <sharathtelu9@gmail.com>
2020-06-30 09:11:25 -0400

    cards shuffling on click

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,30 +24,51 @@ class App extends Component {
 }
 
   handleClick(id) {
+    //Shuffle cards
+    const shuffle = (arr) => {
+      var ctr = arr.length, temp, index;
+      // While there are elements in the array
+      while (ctr > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * ctr);
+        // Decrease ctr by 1
+          ctr--;
+        // And swap the last element with it
+        temp = arr[ctr];
+        arr[ctr] = arr[index];
+        arr[index] = temp;
+      }
+      return arr;
+    }
+    //Loop through characters and check if card with clicked:true is clicked
     for(let character of this.state.initialState.characters) {
       if(character.id === id && character.clicked === true) {
         console.log(character.clicked);
         console.log(this.initialState);
+        //Loop through characters to set clicked equal to false
         for(let character of this.state.initialState.characters) {
           character.clicked = false;
         }
+        //Returning state so rest of the function is not executed
         return this.setState({
           initialState: this.initialState,
           topScore: this.state.topScore
         });
       }
     }
-
+    //Loop through characters and change clicked to true for matched id
     const characters = this.state.initialState.characters.map(character => {
       if(character.id === id) {
         character['clicked'] = true;
       }
       return character;
     });
+    //Update score, topScore and set state
     let score = this.state.initialState.score + 1;
     console.log(score);
     let topScore = this.state.topScore;
     topScore = Math.max(score, topScore); 
+    shuffle(characters);
     this.setState({
       initialState: {
         characters,
